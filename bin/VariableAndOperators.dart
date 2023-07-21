0f205b576aad9eeddd52864f591403208ec15a32
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-07-21 19:20:28 +0600

    different type variable concate

--- bin/VariableAndOperators.dart
+++ bin/VariableAndOperators.dart
@@ -1,11 +1,16 @@
 void main() {
   // Variable Define
-  var name = "Ridoy Paul";
+  var firstName = "Ridoy ";
+  var lastName = "Paul";
   var age = 24;
 
   //Type safety Variable define ---
   String myName = "Ridoy Chandra Paul";
   int myAge = 24;
 
-  print(myName);
+  //Same type variable concate
+  print(firstName+lastName);
+
+  // Different typs variable concate.
+  print("Name: $myName \nAge: $age");
 }
