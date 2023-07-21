46bfb389b247c73262acdeee99a01bfe3c84dc48
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-07-22 01:43:26 +0600

    Extra Practice file added

---
+++ bin/ExtraPractice.dart
@@ -0,0 +1,10 @@
+void main() {
+  var firstName = "Ridoy";
+  var middleName = "Chandra";
+  var lastName = "Paul";
+
+  int age = 24;
+  final gender = "male";
+
+  print("First Name: $firstName \nMiddle Name: $middleName \nLast Name: $lastName \nAge: $age \nGender: $gender");
+}
