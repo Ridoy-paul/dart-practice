4ec647a91a25ffeec3752811d2e7a48efa4c66bc
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-07-23 00:43:43 +0600

    dddd

--- bin/ExtraPractice.dart
+++ bin/ExtraPractice.dart
@@ -6,12 +6,17 @@ void main() {
   int age = 24;
   final gender = "male";
 
-  print("First Name: $firstName \nMiddle Name: $middleName \nLast Name: $lastName \nAge: $age \nGender: $gender");
-
+  print(
+      "First Name: $firstName \nMiddle Name: $middleName \nLast Name: $lastName \nAge: $age \nGender: $gender");
 
   // is and is! operators ------------>>> it explains the true and false.
   double n = 2.2222;
   var nIsInt = n is! int;
   print(nIsInt);
 
+  
+
+  int a = 100;
+  int b = 2;
+  print(a);
 }
---
+++ bin/Loops.dart
@@ -0,0 +1,8 @@
+/*
+
+There are 2 kinds of loops ->>
+1) Definite ( Ex. for )
+2) Indifinite ( Ex. While, do while )
+
+*/
+
