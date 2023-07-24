ec6773c799cd3765f5e1feedbc1d521fa240b02d
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-07-24 22:00:30 +0600

    String end

--- bin/Strings.dart
+++ bin/Strings.dart
@@ -13,4 +13,18 @@ void main() {
 
   // ******** String Methods ****************
   print(firstName.toLowerCase());
+  print(firstName.toUpperCase());
+  print(firstName
+      .trim()); // Returns the string without any leading and trailing whitespace.
+  print(firstName.compareTo(lastName)); // Compares this object to another.
+  print(firstName.replaceAll('Chandra',
+      'Paul')); //Replaces all substrings that match the specified pattern with a given value.
+  print(firstName.split(
+      ',')); //Splits the string at matches of the specified delimiter and returns a list of substrings.
+  print(firstName.substring(2,
+      6)); //এই স্ট্রিংটির সাবস্ট্রিংটি ফেরত দেয় যা startIndex থেকে, inclusive, endIndex, এক্সক্লুসিভ পর্যন্ত প্রসারিত হয়।
+  print(
+      firstName.toString()); // Returns a string representation of this object.
+  print(firstName.codeUnitAt(0)); // Returns the 16-bit UTF-16 code unit at the given index.
+  
 }
