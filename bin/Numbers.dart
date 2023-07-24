67095f7ab4b782bdac459565c9fba0588d556314
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-07-24 21:39:53 +0600

    Number properties and methods end

--- bin/Numbers.dart
+++ bin/Numbers.dart
@@ -12,11 +12,22 @@ int main() {
 
   print('End of Number Properties');
 
-  double num1 = 1000.34;
+  double num1 = 1000.98;
   //Number Methods
   print(num1.abs()); // Returns the absolute value of the number.
   print(num1.ceil()); // Returns the least integer no smaller than the number.
   print(num1.compareTo(1000.34)); // Compares this to other number.
+  print(num1
+      .floor()); // বর্তমান সংখ্যার চেয়ে বড় নয় সবচেয়ে বড় পূর্ণসংখ্যা প্রদান করে।
+  print(num1
+      .remainder(100)); // দুটি সংখ্যা ভাগ করার পরে কাটা অবশিষ্টাংশ ফেরত দেয়।
+  print(num1.round()); // বর্তমান সংখ্যার নিকটতম পূর্ণসংখ্যা প্রদান করে।
+  print(num1.toDouble()); //সংখ্যার সমতুল্য প্রদান করে।
+  print(num1.toInt());
+  print(num1
+      .toString()); // Returns the string equivalent representation of the number.
+  print(num1.truncate()); // Returns an integer after discarding any fractional digits.
+  
 
   return 0;
 }
