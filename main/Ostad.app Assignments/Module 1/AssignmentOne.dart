7c60e387e032304c1ebffaa65a8bf1b3f78aee12
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-08-02 21:24:46 +0600

    dddd

--- main/Ostad.app Assignments/Module 1/AssignmentOne.dart
+++ main/Ostad.app Assignments/Module 1/AssignmentOne.dart
@@ -1,3 +1,22 @@
 void main() {
+  int a = 100;
+  int b = 8;
+
+  print('a = $a, b = $b');
+
+  int addition = a + b;
+  print('Addition: $a + $b = $addition');
+
+  int subtraction = 100 - 8;
+  print('Subtraction: $a - $b = $subtraction');
+
+  int multiplication = a * b;
+  print('Multiplication: $a * $b = $multiplication');
+
+  double division = a / b;
+  print('Division: $a / $b = $division');
+
+  int modulus = a % b;
+  print('Modulus: $a % $b = $modulus');
   
 }
