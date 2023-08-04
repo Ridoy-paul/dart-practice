1921c2c3fcf604b898979e9b10c8f5df484933bd
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-08-04 22:02:04 +0600

    comment updated

--- main/List.dart
+++ main/List.dart
@@ -2,8 +2,20 @@ void main() {
   List<String> studentList = ['Ridoy', 'Abir', 'Rasel'];
   print(studentList);
   print(studentList[0]);
+
+  // Add single item into list.
   studentList.add('Adhihan');
   print(studentList);
+
+  /// Add multiple item into list
   studentList.addAll(['RRR', 'Sajib']);
+  print(studentList);
+
+  ///Remove item from list
+  studentList.remove('Abir');
+  print(studentList);
+
+  /// Remove from list by index.
+  studentList.removeAt(2);
   print(studentList);
 }
