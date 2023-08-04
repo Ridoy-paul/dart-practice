aa2fd158a1d3b703f88b9cf61690cdc5d0921712
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-08-04 21:53:26 +0600

    list updated

--- main/List.dart
+++ main/List.dart
@@ -2,4 +2,8 @@ void main() {
   List<String> studentList = ['Ridoy', 'Abir', 'Rasel'];
   print(studentList);
   print(studentList[0]);
+  studentList.add('Adhihan');
+  print(studentList);
+  studentList.addAll(['RRR', 'Sajib']);
+  print(studentList);
 }
