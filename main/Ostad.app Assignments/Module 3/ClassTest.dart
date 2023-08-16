71cba36c3a6a3c0a23b70000d8b45e9af13580aa
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-08-16 18:49:37 +0600

    ddd

---
+++ main/Ostad.app Assignments/Module 3/ClassTest.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  
+}
+
+class MyClass {
+  
+}
--- main/Records.dart
+++ main/Records.dart
@@ -1,4 +1,12 @@
 void main() {
   var record = ('first', '2nd', '3rd', '3rd', '4th');
   print(record);
+
+  final json = <String, dynamic>{
+    'name': 'Dash',
+    'age': 10,
+    'color': 'blue',
+  };
+
+  print(json);
 }
