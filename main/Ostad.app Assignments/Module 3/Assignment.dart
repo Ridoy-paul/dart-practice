570196f926f638e15f05632ef2d19c16b81d64dc
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-08-18 23:18:43 +0600

    assignment updated

--- main/Ostad.app Assignments/Module 3/Assignment.dart
+++ main/Ostad.app Assignments/Module 3/Assignment.dart
@@ -48,5 +48,5 @@ void main() {
 
   print("Car 1: ${car1.getBrand()} ${car1.getModel()} ${car1.getYear()} Miles: ${car1.getMilesDriven()} Age: ${car1.getAge()}");
   print("Car 2: ${car2.getBrand()} ${car2.getModel()} ${car2.getYear()} Miles: ${car2.getMilesDriven()} Age: ${car2.getAge()}");
-  print("Car 3: ${car3.getBrand()} ${car3.getModel()} ${car3.getYear()} Miles: ${car3.getMilesDriven()} Age: ${car3.getAge()} Total number of cars created: ${Car.numberOfCars}");
+  print("Car 3: ${car3.getBrand()} ${car3.getModel()} ${car3.getYear()} Miles: ${car3.getMilesDriven()} Age: ${car3.getAge()}\nTotal number of cars created: ${Car.numberOfCars}");
 }
