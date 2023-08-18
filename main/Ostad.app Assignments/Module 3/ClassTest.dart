f884e91bf42379956915b0b44eb9a503184da2fc
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-08-18 19:52:33 +0600

    module 3 assignment running

--- main/Ostad.app Assignments/Module 3/ClassTest.dart
+++ main/Ostad.app Assignments/Module 3/ClassTest.dart
@@ -1,7 +1,53 @@
-void main(List<String> args) {
+void main() {
+  Person ridoy = Person();
+  ridoy.name = "Ridoy Paul";
+  ridoy.age = 24;
+  ridoy.address = "Mirpur, Dhaka";
+  ridoy.gender = 'Male';
+
+  ridoy.printInfo();
+
+  Person arif = Person();
+  arif.name = "Ariful Sohan";
+  arif.age = 24;
+  arif.address = 'Mirpur 1, Dhaka';
+  arif.gender = "Male";
+  arif.printInfo();
+
+  StudentInfo rp = StudentInfo();
   
 }
 
-class MyClass {
+class StudentInfo {
+  String name = '';
+  int? ID;
+  String className = '';
+
+  StudentInfo() {
+    print("Student Name: $name\n ID: $ID\n Class Name: $className");
+  }
+}
+
+class Person {
+  String name = '';
+  int age = 0;
+  String address = '';
+  String gender = '';
+
+  // Constructor.
+  Person() {
+    print("This is the Constructor");
+  }
+
+  void printInfo() {
+    print(this.name);
+    print(this.age);
+    print(this.address);
+    print(this.gender);
+  }
+}
+
+
+void name() {
   
 }
