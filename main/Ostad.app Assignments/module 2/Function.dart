25d36758c69ba37d9d770c43101e5d4747127b6e
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-08-20 20:46:33 +0600

    optional parameter updated

--- main/Ostad.app Assignments/module 2/Function.dart
+++ main/Ostad.app Assignments/module 2/Function.dart
@@ -1,7 +1,16 @@
 void main() {
   addition(30, 50);
+  var ridoy = printInfo("Ridoy Paul", 'Very Good');
+  print(ridoy);
+  var op = printInfo("Optional Parameter");
+  print(op);
 }
 
 void addition(int a, int b) {
   print(a + b);
+}
+
+String printInfo(String name, [String message = 'Null']) {
+  // optional parameter in function
+  return "Name: $name, Message: $message";
 }
