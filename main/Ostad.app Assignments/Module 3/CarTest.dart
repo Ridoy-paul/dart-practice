0e550f11dfa7073ed8a09ea658bbf4be9011d15d
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-08-24 19:09:35 +0600

    Test Updated

---
+++ main/Ostad.app Assignments/Module 3/CarTest.dart
@@ -0,0 +1,23 @@
+class Car {
+  String? carModel;
+  String? brand;
+  double? price;
+  String? _engineNumber;
+
+  Car(String carModel, String brand, double price, String engineNumber) {
+    this.carModel = carModel;
+    this.brand = brand;
+    this.price = price;
+    this._engineNumber = engineNumber;
+  }
+
+  void carInfo() {
+    print(
+        "Car Brand: ${brand} \nModel: ${carModel} \nPrice: ${price}\nEngine Number: ${_engineNumber}");
+  }
+}
+
+void man() {
+  Car bmw = Car("X7", "BMW", 1200000, "34553");
+  bmw.carInfo();
+}
